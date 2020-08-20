a85bb03af15f8164af4917b62921f90bf9bcbd28
Bertrand Bougon <bertrand.bougon@gmail.com>
2020-08-20 08:36:48 +0200

    Add a injected junit rule to separate transactions in test method,\n + Add docker compose file to boot MySQL

--- src/test/java/com/micheliknechtel/petshop/data/repository/AnimalRepositoryIntegrationTest.java
+++ src/test/java/com/micheliknechtel/petshop/data/repository/AnimalRepositoryIntegrationTest.java
@@ -2,45 +2,56 @@ package com.micheliknechtel.petshop.data.repository;
 
 
 import com.micheliknechtel.petshop.domain.Animal;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.support.TransactionSynchronizationAdapter;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
+import javax.persistence.EntityTransaction;
+
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(classes = TestApplicationContext.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class AnimalRepositoryIntegrationTest {
-
-
-
-    @Autowired
-    private TestEntityManager entityManager;
 
     @Autowired
     private AnimalRepository animalRepository;
 
+    @Autowired
+    @Rule
+    public WithEntityManager withEntityManager;
+
     @Test
     public void testFindByName() {
-
         // setup data scenario
-        Animal aAnimal = new Animal();
-        aAnimal.setName("Mia");
+        withEntityManager.doInTransaction(em -> {
 
-        // save test data
-        entityManager.persist(aAnimal);
+            Animal aAnimal = new Animal();
+            aAnimal.setName("Mia");
+            animalRepository.save(aAnimal);
+            aAnimal.setGender("Female");
+        });
 
-        // Find an inserted record
-        Animal foundAnimal= animalRepository.findByName("Mia");
+        withEntityManager.doInTransaction(em -> {
+            Animal foundAnimal= animalRepository.findByName("Mia");
 
-        assertThat(foundAnimal.getName(), is(equalTo("Mia")));
+            assertThat(foundAnimal.getName(), is(equalTo("Mia")));
+            assertThat(foundAnimal.getId(), is(notNullValue()));
+            assertThat(foundAnimal.getGender(), is("Female"));
+        });
     }
 
 
---
+++ src/test/java/com/micheliknechtel/petshop/data/repository/TestApplicationContext.java
@@ -0,0 +1,10 @@
+package com.micheliknechtel.petshop.data.repository;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.micheliknechtel.petshop")
+public class TestApplicationContext {
+
+}
